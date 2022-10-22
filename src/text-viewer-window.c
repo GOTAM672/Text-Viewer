e9bdc4af4118a1352ec15667c0ba10990068af1a
Gotam Gorabh <gautamy672@gmail.com>
2022-10-23 05:28:46 +0530

    adding scrolled window

--- src/text-viewer-window.c
+++ src/text-viewer-window.c
@@ -28,7 +28,7 @@ struct _TextViewerWindow
 
   /* Template widgets */
   GtkHeaderBar        *header_bar;
-  GtkLabel            *label;
+  GtkTextView         *main_text_view;
 };
 
 G_DEFINE_FINAL_TYPE (TextViewerWindow, text_viewer_window, ADW_TYPE_APPLICATION_WINDOW)
@@ -40,7 +40,7 @@ text_viewer_window_class_init (TextViewerWindowClass *klass)
 
   gtk_widget_class_set_template_from_resource (widget_class, "/com/example/TextViewer/text-viewer-window.ui");
   gtk_widget_class_bind_template_child (widget_class, TextViewerWindow, header_bar);
-  gtk_widget_class_bind_template_child (widget_class, TextViewerWindow, label);
+  gtk_widget_class_bind_template_child (widget_class, TextViewerWindow, main_text_view);
 }
 
 static void
